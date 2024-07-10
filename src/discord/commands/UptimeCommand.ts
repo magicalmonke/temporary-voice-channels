4df6a83dcc3c39162bd830b26d2f2b70555b3068
POGROTH THE HYPED <82341064+magicalmonke@users.noreply.github.com>
2024-07-10 18:55:11 +0200

    Update UptimeCommand.ts

--- src/discord/commands/UptimeCommand.ts
+++ src/discord/commands/UptimeCommand.ts
@@ -1,15 +1,15 @@
-import { SlashCommandBuilder } from "discord.js";
+import { inlineCode, SlashCommandBuilder } from "discord.js";
 import { Command } from "../types";
 import prettyMilliseconds from "pretty-ms";
 
 export const UptimeCommand: Command = {
     metadate: new SlashCommandBuilder()
         .setName("uptime")
-        .setDescription("Check the bot's uptime")
+        .setDescription("Shows how long the bot has been running.")
         .toJSON(),
     async execute(interaction) {
         await interaction.reply({
-            content: `The bot has been running for: ${prettyMilliseconds(interaction.client.uptime, { verbose: true })}`,
+            content: `The bot has been running for: ${inlineCode(prettyMilliseconds(interaction.client.uptime, { verbose: true }))}`,
             ephemeral: true
         });
     }
