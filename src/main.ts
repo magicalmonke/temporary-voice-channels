c558be5eae736f0645eff3ab31ea96aeca9c6844
POGROTH THE HYPED <82341064+magicalmonke@users.noreply.github.com>
2024-07-10 18:47:45 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { Client, GatewayIntentBits } from "discord.js";
 import "dotenv/config";
 import { ClientReadyListener } from "./discord/listeners/ClientReadyListener";
 import { Logger } from "tslog";
+import { InteractionCreateListener } from "./discord/listeners/InteractionCreateListener";
 
 export const logger = new Logger();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const listeners = [ClientReadyListener];
+const listeners = [ClientReadyListener, InteractionCreateListener];
 for (const listener of listeners) {
     if (listener.once) {
         client.once(listener.event, listener.execute);
