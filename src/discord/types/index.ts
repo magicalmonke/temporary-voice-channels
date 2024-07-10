1bbc6585c3e405c79b52a101fe0ea96263b3e1f3
POGROTH THE HYPED <82341064+magicalmonke@users.noreply.github.com>
2024-07-10 18:28:39 +0200

    Add Command interface

--- src/discord/types/index.ts
+++ src/discord/types/index.ts
@@ -1,7 +1,12 @@
-import { ClientEvents } from "discord.js";
+import { ClientEvents, CommandInteraction, RESTPostAPIChatInputApplicationCommandsJSONBody } from "discord.js";
 
 export interface Listener {
     event: keyof ClientEvents;
     once?: boolean;
     execute(...args: unknown[]): Promise<void> | void;
+}
+
+export interface Command {
+    metadate: RESTPostAPIChatInputApplicationCommandsJSONBody;
+    execute(interaction: CommandInteraction): Promise<void>;
 }
