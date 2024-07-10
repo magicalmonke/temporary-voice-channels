fb275e5fe55809ad32f09a66f0db803c4ff38fed
POGROTH THE HYPED <82341064+magicalmonke@users.noreply.github.com>
2024-07-10 18:47:17 +0200

    Register slash commands

--- src/discord/listeners/ClientReadyListener.ts
+++ src/discord/listeners/ClientReadyListener.ts
@@ -1,11 +1,17 @@
 import { Client, Events } from "discord.js";
 import { Listener } from "../types";
 import { logger } from "../../main";
+import { commands } from "./InteractionCreateListener";
 
 export const ClientReadyListener: Listener = {
     event: Events.ClientReady,
     once: true,
     execute: async (client: Client<true>) => {
         logger.info(`Ready! Logged in as ${client.user.tag}`);
+
+        if (process.env.REGISTER_DISCORD_COMMANDS?.toLowerCase() === "true") {
+            logger.info("Registering slash commands...");
+            await client.application.commands.set(commands.map(command => command.metadate));
+        }
     },
 };
