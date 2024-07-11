d820b8b3e74394e69db859cfeeab871914fd0183
POGROTH THE HYPED <82341064+magicalmonke@users.noreply.github.com>
2024-07-11 14:19:49 +0200

    Refactor SetupCommand

--- src/discord/commands/SetupCommand.ts
+++ src/discord/commands/SetupCommand.ts
@@ -1,4 +1,4 @@
-import { type ChatInputCommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+import { ChannelType, type ChatInputCommandInteraction, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
 import type { Command } from "../types";
 import { prisma } from "../../main";
 import { TemplateChannelType } from "@prisma/client";
@@ -20,41 +20,25 @@ export const SetupCommand: Command = {
                 .setDescription("Setup a clone “Join To Create” channel which is cloned when a user joins it")
         )
 		.toJSON(),
-	async execute(interaction: ChatInputCommandInteraction) {
-        // biome-ignore lint/style/noNonNullAssertion: DM permission is set to false
-        const guild = interaction.guild!;
-        // biome-ignore lint/style/noNonNullAssertion: It's an ChatInputCommandInteraction so it has a channel
-        const channel = interaction.channel!;
+        async execute(interaction: ChatInputCommandInteraction) {
+            // biome-ignore lint/style/noNonNullAssertion: DM permission is set to false
+            const guild = interaction.guild!;
+            // biome-ignore lint/style/noNonNullAssertion: ChatInputCommandInteraction has to be in a channel
+            const channel = interaction.channel!;
 
-        const subcommand = interaction.options.getSubcommand();
-        switch (subcommand) {
-            case "default": {
-                await prisma.templateChannel.upsert({
-                    where: { id: channel.id },
-                    update: { type: TemplateChannelType.DEFAULT },
-                    create: {
-                        id: channel.id,
-                        guild: {
-                            connectOrCreate: {
-                                where: { id: guild.id },
-                                create: { id: guild.id }
-                            }
-                        },
-                        type: TemplateChannelType.DEFAULT,
-                    }
-                });
-
+            if (channel.type !== ChannelType.GuildVoice) {
                 await interaction.reply({
-                    content: "Default setup",
+                    content: "Please select a voice channel.",
                     ephemeral: true,
                 });
-
-                break;
+                return;
             }
-            case "clone": {
+
+            const subcommand = interaction.options.getSubcommand();
+            const setupTemplateChannel = async (type: TemplateChannelType, message: string) => {
                 await prisma.templateChannel.upsert({
                     where: { id: channel.id },
-                    update: { type: TemplateChannelType.CLONE },
+                    update: { type: type },
                     create: {
                         id: channel.id,
                         guild: {
@@ -63,17 +47,23 @@ export const SetupCommand: Command = {
                                 create: { id: guild.id }
                             }
                         },
-                        type: TemplateChannelType.CLONE,
+                        type: type,
                     }
                 });
 
                 await interaction.reply({
-                    content: "Clone setup",
+                    content: message,
                     ephemeral: true,
                 });
+            };
 
-                break;
+            switch (subcommand) {
+                case "default":
+                    await setupTemplateChannel(TemplateChannelType.DEFAULT, "Default setup");
+                    break;
+                case "clone":
+                    await setupTemplateChannel(TemplateChannelType.CLONE, "Clone setup");
+                    break;
             }
         }
-	},
 };
